28054197216612682423c44320f2cac940d3e9cd
eliferrtenn <69215796+eliferrtenn@users.noreply.github.com>
2023-10-24 14:05:10 +0300

    Update ListVersiyonResponse.cs

--- VardabitCore.Business/Models/Responses/VersiyonResponses/ListVersiyonResponse.cs
+++ VardabitCore.Business/Models/Responses/VersiyonResponses/ListVersiyonResponse.cs
@@ -3,7 +3,6 @@ namespace VardabitCore.Business.Models.Responses.VersiyonResponses
 {
 	public class ListVersiyonResponse
 	{
-        public long ID { get; set; }
         public float DepolamaKapasitesi { get; set; }
         public float Fiyat { get; set; }
         public string StokDurumu { get; set; }
