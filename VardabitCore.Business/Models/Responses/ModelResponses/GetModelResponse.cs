961afaf5f3e7c68a36eae6fc213311dca6b66b09
eliferrtenn <69215796+eliferrtenn@users.noreply.github.com>
2023-10-24 14:19:26 +0300

    Update GetModelResponse.cs

--- VardabitCore.Business/Models/Responses/ModelResponses/GetModelResponse.cs
+++ VardabitCore.Business/Models/Responses/ModelResponses/GetModelResponse.cs
@@ -2,8 +2,7 @@
 {
 	public class GetModelResponse
 	{
-        public long ID { get; set; }
-        public long MarkaID { get; set; }
+        public string MarkaAd { get; set; }
         public string ModelAd { get; set; }
     }
 }
