6bf31b03306809e99b1f65b4296c349337bdd780
eliferrtenn <69215796+eliferrtenn@users.noreply.github.com>
2023-10-24 14:23:49 +0300

    Update ListMarkaResponses.cs

--- VardabitCore.Business/Models/Responses/MarkaResponses/ListMarkaResponses.cs
+++ VardabitCore.Business/Models/Responses/MarkaResponses/ListMarkaResponses.cs
@@ -3,7 +3,6 @@ namespace VardabitCore.Business.Models.Responses.MarkaResponses
 {
 	public class ListMarkaResponses
 	{
-        public long ID { get; set; }
         public string MarkaAd { get; set; }
     }
 }
