f505a73c347b305e875949da3e5698bfc89a3ebb
eliferrtenn <69215796+eliferrtenn@users.noreply.github.com>
2023-10-24 13:28:56 +0300

    Update AddSiparisRequest.cs

--- VardabitCore.Business/Models/Requests/SiparisRequests/AddSiparisRequest.cs
+++ VardabitCore.Business/Models/Requests/SiparisRequests/AddSiparisRequest.cs
@@ -3,7 +3,6 @@ namespace VardabitCore.Business.Models.Requests.SiparisRequests
 {
 	public class AddSiparisRequest
 	{
-        public long ID { get; set; }
         public DateTime SiparisTarih { get; set; }
         public float ToplamFiyat { get; set; }
         public int SiparisDurum { get; set; }
