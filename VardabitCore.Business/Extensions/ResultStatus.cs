abcc39dcc96e1b2e2e2777f388e3ebd31163bf3a
eliferrtenn <69215796+eliferrtenn@users.noreply.github.com>
2023-10-24 11:38:32 +0300

    Delete VardabitCore.Business/Extensions directory

--- VardabitCore.Business/Extensions/ResultStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VardabitCore.Business.Extensions
-{
-	public class ResultStatus
-	{
-        public const string Success = "Başarılı!";
-        public const string Error = "Başarısız";
-    }
-}
