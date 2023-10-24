179edc001b99ce3f713a17599578fea0f54fae51
eliferrtenn <69215796+eliferrtenn@users.noreply.github.com>
2023-10-24 13:33:39 +0300

    Update AddVersiyonRequest.cs

--- VardabitCore.Business/Models/Requests/VersiyonRequests/AddVersiyonRequest.cs
+++ VardabitCore.Business/Models/Requests/VersiyonRequests/AddVersiyonRequest.cs
@@ -3,7 +3,6 @@ namespace VardabitCore.Business.Models.Requests.VersiyonRequests
 {
 	public class AddVersiyonRequest
 	{
-        public long ID { get; set; }
         public float DepolamaKapasitesi { get; set; }
         public float Fiyat { get; set; }
         public string StokDurumu { get; set; }
